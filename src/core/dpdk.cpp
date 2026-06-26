b740a03177e3041c4787d0cbfed2062ce7647423
alterhoodie <manishimmi2k3@gmail.com>
2026-06-26 13:50:05 +0000

    fix: not failing at promiscuous failure

--- src/core/dpdk.cpp
+++ src/core/dpdk.cpp
@@ -108,7 +108,6 @@ int Dpdk::portInit(uint16_t port, struct rte_mempool *mbuf_pool)
     retval = rte_eth_promiscuous_enable(port);
     printf("promiscuous enable = %d (%s)\n",
    retval, retval < 0 ? rte_strerror(-retval) : "OK");
-    if (retval != 0) return retval;
 
 
     rte_eth_stats_reset(port);
